36e4e96dbbbbea75e3cb945f42b59a3436b4dd73
Oleksii Bulygin <bulygin@datarootlabs.com>
2020-09-11 18:42:42 +0300

    Work in progress

--- main2.py
+++ main2.py
@@ -14,20 +14,25 @@ retrieval_model = Retriever()
 
 retrieval_model.to(CUDA)
 optimizer = torch.optim.Adam(retrieval_model.parameters(), lr=0.0001)
-optimizer
-loss_fn = torch.nn.L1Loss().to(CUDA)
+loss_fn = torch.nn.L1Loss(reduction='sum').to(CUDA)
 
 encoder = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
-data = load_data('data/persona_self_original.json')
-train_loader, train_utts, val_loader, val_utts = get_loaders(data, encoder, 32, 64)
+
+data = load_data('data/persona_dev.json')
+train_loader, train_utts, val_loader, val_utts = get_loaders(data, encoder, 64, 64)
 
 def run(model):
     def train_step(engine, batch):
         model.train()
         optimizer.zero_grad()
-        x, not_y, y = (encoder.encode(i[0], convert_to_tensor=True).to(CUDA) for i in batch)
+        x, not_ys, y = batch
+        x = encoder.encode(x[0], convert_to_tensor=True).to(CUDA)
+        not_ys = [encoder.encode(not_y, convert_to_tensor=True).to(CUDA) for not_y in not_ys]
+        y = encoder.encode(y, convert_to_tensor=True).to(CUDA)
         yhat = model(x)
         loss = loss_fn(yhat, y)
+        gains = torch.sum(torch.stack([loss_fn(not_y, y) for not_y in not_ys])) / 20.0
+        loss /= gains
         loss.backward()
         optimizer.step()
         return loss.item()
@@ -35,7 +40,10 @@ def run(model):
     def eval_step(engine, batch):
         model.eval()
         with torch.no_grad():
-            x, not_y, y = (encoder.encode(i[0], convert_to_tensor=True).to(CUDA) for i in batch)
+            x, not_ys, y = batch
+            x = encoder.encode(x[0], convert_to_tensor=True).to(CUDA)
+            not_ys = [encoder.encode(not_y, convert_to_tensor=True).to(CUDA) for not_y in not_ys]
+            y = encoder.encode(y, convert_to_tensor=True).to(CUDA)
             yhat = model(x)
             return yhat, y
     
@@ -46,33 +54,33 @@ def run(model):
     evaluator.logger = setup_logger('evaluator')
     
     l1 = Loss(loss_fn)
-    r1 = Recall(average=False)
-    
     
     l1.attach(evaluator, 'l1')
-    r1.attach(evaluator, 'r1')
+    
+    @trainer.on(Events.ITERATION_COMPLETED(every=1))
+    def log_training(engine):
+        batch_loss = engine.state.output
+        lr = optimizer.param_groups[0]['lr']
+        e = engine.state.epoch
+        n = engine.state.max_epochs
+        i = engine.state.iteration
+        print("Epoch {}/{} : {} - batch loss: {}, lr: {}".format(e, n, i, batch_loss, lr))
+
     
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_training_results(engine):
         evaluator.run(train_loader)
         metrics = evaluator.state.metrics
-        los = metrics['l1']
-        avg_rec = metrics['r1']
         print(f"Training Results - Epoch: {engine.state.epoch} " 
-              f"L1: {metrics['l1']:.2f} R1: {metrics['r1']:.2f}")
+              f"L1: {metrics['l1']:.2f}")
         
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_validation_results(engine):
         evaluator.run(val_loader)
         metrics = evaluator.state.metrics
         print(f"Validation Results - Epoch: {engine.state.epoch} "
-              f"L1: {metrics['l1']:.2f} R1: {metrics['r1']:.2f}")
+              f"L1: {metrics['l1']:.2f}")
 
-
-
-    
     trainer.run(train_loader, max_epochs=10)
-
-
         
 run(retrieval_model)
